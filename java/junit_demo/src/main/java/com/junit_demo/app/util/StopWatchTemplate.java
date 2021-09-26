ded984155b8763dbe7bb0d0d691ef430adb871e7
清风 <qiupanwei@aliyun.com>
2021-09-26 11:52:50 +0800

    <feat>[java8] 增加工具类，以及日志打印

---
+++ java/junit_demo/src/main/java/com/junit_demo/app/util/StopWatchTemplate.java
@@ -0,0 +1,37 @@
+package com.junit_demo.app.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.StopWatch;
+
+import java.util.function.Supplier;
+
+/**
+ * 计时器模板类
+ */
+public class StopWatchTemplate {
+
+	private static final Logger log = LoggerFactory.getLogger(StopWatchTemplate.class);
+	
+	public static void start(String taskId, Runnable runnable) {
+		StopWatch watch = new StopWatch(taskId);
+		watch.start();
+		try {
+			runnable.run();
+		} finally {
+			watch.stop();
+			log.info("StopWatch '{}': running time = {} ms", watch.getId(), watch.getTotalTimeMillis());
+		}
+	}
+	
+	public static <T> T start(String taskId, Supplier<T> supplier) {
+		StopWatch watch = new StopWatch(taskId);
+		watch.start();
+		try {
+			return supplier.get();
+		} finally {
+			watch.stop();
+			log.info("StopWatch '{}': running time = {} ms", watch.getId(), watch.getTotalTimeMillis());
+		}
+	}
+}
