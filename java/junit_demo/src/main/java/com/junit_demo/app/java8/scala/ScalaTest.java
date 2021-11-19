5807f912c79ef9a2e856747ebe78610d5ce0f9c8
清风 <carl_panwei@163.com>
2021-11-19 18:34:37 +0800

    <feat>[scala]

---
+++ java/junit_demo/src/main/java/com/junit_demo/app/java8/scala/ScalaTest.java
@@ -0,0 +1,14 @@
+package com.junit_demo.app.java8.scala;
+
+/**
+ * <p> @author     :  清风
+ * <p> description :
+ *
+ *      Scala是一种混合了面向对象和函数式编程的语言,它常常被看作Java的一种替代语言.
+ *
+ *
+ *
+ * <p> create date :  2021/11/19 14:51
+ */
+public class ScalaTest {
+}
