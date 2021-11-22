98373869340015ee468246aa005e73c76317dcf5
weice <carl_panwei@163.com>
2021-11-22 23:51:53 +0800

    <feat>[stream]同一个流，利用并发执行不同操作

--- java/junit_demo/src/main/java/com/junit_demo/app/java8/collector/MenuCollectTest.java
+++ java/junit_demo/src/main/java/com/junit_demo/app/java8/collector/MenuCollectTest.java
@@ -3,12 +3,15 @@ package com.junit_demo.app.java8.collector;
 import com.alibaba.fastjson.JSON;
 import com.junit_demo.app.annotion.ListSource;
 import com.junit_demo.app.java8.model.Menu;
+import com.junit_demo.app.java8.stream.StreamForker;
 import com.junit_demo.app.util.PrintTool;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class MenuCollectTest {
 
@@ -26,6 +29,22 @@ public class MenuCollectTest {
     public void menuCollect2(List<Menu> menus) {
         List<Menu> collect = menus.stream().collect(new ToListCollector<>());
         PrintTool.print(JSON.toJSONString(collect));
+    }
+
+    @ParameterizedTest
+    @ListSource(resource = "menu.json", clazz = Menu.class)
+    @DisplayName("同一个流进行不同操作")
+    public void pallMenuCollect(List<Menu> menus) {
+        Stream<Menu> stream = menus.stream();
+        StreamForker<Menu> menuStreamForker = new StreamForker<>(stream);
+        StreamForker.Results results = menuStreamForker
+                .fork("list", a -> a.collect(Collectors.toList()))
+                .fork("getAllName", a -> a.map(Menu::getName).collect(Collectors.toList()))
+                .getResults();
+        List<Menu> list = (List<Menu>)results.get("list");
+        List<String> names = (List<String>)results.get("getAllName");
+        PrintTool.print(JSON.toJSONString(list));
+        PrintTool.print(JSON.toJSONString(names));
     }
 
 }
