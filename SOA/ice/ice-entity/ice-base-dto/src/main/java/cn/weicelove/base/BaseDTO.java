86db8f9542538276d8aebd16bf44cff8a1c4230e
清风 <carl_panwei@163.com>
2021-12-29 19:27:52 +0800

    <feat>[微服务搭建]DTO,VO

---
+++ SOA/ice/ice-entity/ice-base-dto/src/main/java/cn/weicelove/base/BaseDTO.java
@@ -0,0 +1,9 @@
+package cn.weicelove.base;
+
+/**
+ * <p> @author     :  清风
+ * <p> description :  DTO代表服务层需要接收的数据和返回的数据
+ * <p> create date :  2021/12/29 14:47
+ */
+public class BaseDTO {
+}
---
+++ SOA/ice/ice-entity/ice-base-vo/src/main/java/cn/weicelove/base/BaseVO.java
@@ -0,0 +1,9 @@
+package cn.weicelove.base;
+
+/**
+ * <p> @author     :  清风
+ * <p> description :  VO代表展示层需要显示的数据
+ * <p> create date :  2021/12/29 14:46
+ */
+public class BaseVO {
+}
