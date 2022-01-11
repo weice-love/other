319d7cc55b3fea2ce60b4504732bef95a1dc70d3
weice <carl_panwei@163.com>
2022-01-12 01:05:30 +0800

    <feat>[db配置]调整依赖

--- SOA/ice/ice-config/ice-config-db/src/main/java/cn/weicelove/config/datasource/SlaveDatabase.java
+++ SOA/ice/ice-config/ice-config-db/src/main/java/cn/weicelove/config/datasource/SlaveDatabase.java
@@ -16,8 +16,8 @@ public class SlaveDatabase {
     /**
      * 读数据源配置01
      */
-    @Bean(name = "read-slave01")
-    @ConfigurationProperties(prefix = "spring.datasource.read-slave01")
+    @Bean(name = "read-slave01-datasource")
+    @ConfigurationProperties(prefix = "spring.datasource.read-slave01-datasource")
     public DataSource read01DataSource() {
         return DataSourceBuilder.create().build();
     }
@@ -25,8 +25,8 @@ public class SlaveDatabase {
     /**
      * 读数据源配置02
      */
-    @Bean(name = "read-slave02")
-    @ConfigurationProperties(prefix = "spring.datasource.read-slave02")
+    @Bean(name = "read-slave02-datasource")
+    @ConfigurationProperties(prefix = "spring.datasource.read-slave02-datasource")
     public DataSource read02DataSource() {
         return DataSourceBuilder.create().build();
     }
